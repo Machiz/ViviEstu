4fd520a68f5331258d63e0cf167d8205762eebf7
DavidA <dserrudocaina@gmail.com>
2025-10-10 18:38:50 -0300

    feat: finalizacion API alojamientos

---
+++ src/main/java/com/ViviEstu/mapper/AlojamientoMapper.java
@@ -0,0 +1,32 @@
+package com.ViviEstu.mapper;
+
+import com.ViviEstu.model.dto.request.AlojamientoRequestDTO;
+import com.ViviEstu.model.dto.response.AlojamientoResponseDTO;
+import com.ViviEstu.model.entity.Alojamiento;
+import lombok.AllArgsConstructor;
+import org.modelmapper.ModelMapper;
+import org.springframework.stereotype.Component;
+
+import java.util.List;
+
+@Component
+@AllArgsConstructor
+public class AlojamientoMapper {
+
+    private final ModelMapper mapper = new ModelMapper();
+
+    public Alojamiento convertToEntity(AlojamientoRequestDTO dto) {
+        return mapper.map(dto, Alojamiento.class);
+    }
+
+    public AlojamientoResponseDTO convertToDTO(Alojamiento alojamiento) {
+        AlojamientoResponseDTO dto = mapper.map(alojamiento, AlojamientoResponseDTO.class);
+        dto.setPropietario(alojamiento.getPropietario().getNombre());
+        dto.setZona(alojamiento.getDistrito().getNombre());
+        return dto;
+    }
+
+    public List<AlojamientoResponseDTO> convertToListDTO(List<Alojamiento> list) {
+        return list.stream().map(this::convertToDTO).toList();
+    }
+}

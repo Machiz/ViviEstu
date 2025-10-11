828b033d4ee44236f67b04e64125d7962e0dd9c8
DavidA <dserrudocaina@gmail.com>
2025-10-11 17:44:49 -0500

    Ajuste PropietariosRequest

--- src/main/java/com/ViviEstu/model/dto/request/PropietariosRequestDTO.java
+++ src/main/java/com/ViviEstu/model/dto/request/PropietariosRequestDTO.java
@@ -7,19 +7,20 @@ import lombok.Data;
 @Data
 public class PropietariosRequestDTO {
 
-    @NotBlank
+    @NotBlank(message = "El nombre no puede estar vacío")
     private String nombre;
 
-    @Email
+    @Email(message = "El correo debe tener un formato válido")
+    @NotBlank(message = "El correo no puede estar vacío")
     private String correo;
 
-    @NotBlank
+    @NotBlank(message = "La contraseña no puede estar vacía")
     private String contrasenia;
 
-    @NotBlank
+    @NotBlank(message = "El teléfono no puede estar vacío")
     private String telefono;
 
-    @NotBlank
+    @NotBlank(message = "La URL del perfil no puede estar vacía")
     private String urlPerfil;
 
     private Integer dni;
